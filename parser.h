d347f826d26f4b6b775a0a0aa8693682d1d44f46
lukaskurnia <lukasjonathan99@gmail.com>
2018-11-19 12:58:12 +0700

    parser.h : add aturan produksi

--- parser.h
+++ parser.h
@@ -5,9 +5,11 @@
 #include "boolean.h"
 
 /*
-S -> T | S+T | S-T plusMinus
-T -> F | T*F | T/F kaliBagi
+S -> T | S+T | S-T calculate
+T -> P | T*P | T/P KaliBagi
+P -> F | F^P
 F -> (S) | A sign
+A -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | AA number
 */
 
 int isNumber(char strIn);
