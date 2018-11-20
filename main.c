625830d08e89e43a765fa13bd8453b7943c58dd8
jrandiny <jrandiny2@gmail.com>
2018-11-20 09:54:34 +0700

    Fix some crossplatform issue

--- main.c
+++ main.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include "parser.h"
-#include "signal.h"
 
 #ifdef __linux__
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdlib.h>
 #endif
 
 int main()
 {
-    /* KAMUS */
-    int status;
-    int idx;
-    float res;
-    char *strIn;
+  /* KAMUS */
+  int status;
+  int idx;
+  float res;
+  #ifdef __linux__
+  char *strIn;
+  strIn = NULL;
+  #else
+  char strIn[50];
+  #endif
 
 
-    /* ALGORITMA */
-    signal(SIGINT, SIG_IGN);
+  /* ALGORITMA */
+  printf("\n");
+  printf("                  888                   888          888                     \n");
+  printf("                  888                   888          888                     \n");
+  printf("                  888                   888          888                     \n");
+  printf(" .d8888b  8888b.  888  .d8888b 888  888 888  8888b.  888888  .d88b.  888d888 \n");
+  printf("d88P'        '88b 888 d88P'    888  888 888     '88b 888    d88''88b 888P'   \n");
+  printf("888      .d888888 888 888      888  888 888 .d888888 888    888  888 888     \n");
+  printf("Y88b.    888  888 888 Y88b.    Y88b 888 888 888  888 Y88b.  Y88..88P 888     \n");
+  printf(" 'Y8888P 'Y888888 888  'Y8888P  'Y88888 888 'Y888888  'Y888  'Y88P'  888     \n\n");
 
-    printf("\n");
-    printf("                  888                   888          888                     \n");
-    printf("                  888                   888          888                     \n");
-    printf("                  888                   888          888                     \n");
-    printf(" .d8888b  8888b.  888  .d8888b 888  888 888  8888b.  888888  .d88b.  888d888 \n");
-    printf("d88P'        '88b 888 d88P'    888  888 888     '88b 888    d88''88b 888P'   \n");
-    printf("888      .d888888 888 888      888  888 888 .d888888 888    888  888 888     \n");
-    printf("Y88b.    888  888 888 Y88b.    Y88b 888 888 888  888 Y88b.  Y88..88P 888     \n");
-    printf(" 'Y8888P 'Y888888 888  'Y8888P  'Y88888 888 'Y888888  'Y888  'Y88P'  888     \n\n");
+  do{
 
-    printf("Silahkan masukkan input: (ketik 'exit' untuk mengakhiri program)\n");
+
+    printf("\nSilahkan masukkan input: (ketik 'exit' untuk mengakhiri program)\n");
 
     #ifdef __linux__
+    free(strIn);
     strIn = readline(">> ");
     add_history(strIn);
     #else
@@ -40,40 +47,35 @@ int main()
     scanf("%s", strIn);
     #endif
 
-    while(strcmp(strIn,"exit") != 0){
-
-        status = 0;
-        /*
-        status = 0 ekspresi valid
-        status = 1 syntax errror
-        status = 2 math error
-        */
-        idx = 0;
-        res = 0;
-        plusMinus(strIn, &idx, &res, &status);
-        printf(">> ");
-        if (status==0 && strIn[idx]=='\0'){
-            printf("%.2f\n",res);
-        }else{
-            if (status == 2 && strIn[idx] =='\0'){
-                printf("Math Error\n");
-            }
-            else{
-                printf("Syntax Error\n");
-            }
+    if(strcmp(strIn,"exit")!=0){
+      status = 0;
+      /*
+      status = 0 ekspresi valid
+      status = 1 syntax errror
+      status = 2 math error
+      */
+      idx = 0;
+      res = 0;
+      plusMinus(strIn, &idx, &res, &status);
+      printf(">> ");
+      if (status==0 && strIn[idx]=='\0'){
+        printf("%.2f\n",res);
+      }else{
+        if (status == 2 && strIn[idx] =='\0'){
+          printf("Math Error\n");
         }
-
-        printf("\nSilahkan masukkan input: (ketik 'exit' untuk mengakhiri program)\n");
-
-        #ifdef __linux__
-        strIn = readline(">> ");
-        add_history(strIn);
-        #else
-        printf(">> ");
-        scanf("%s", strIn);
-        #endif
+        else{
+          printf("Syntax Error\n");
+        }
+      }
     }
 
-    return 0;
+  }while(strcmp(strIn,"exit") != 0);
+
+  #ifdef __linux__
+  free(strIn);
+  #endif
+
+  return 0;
 
 }
